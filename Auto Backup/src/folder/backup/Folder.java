e2a14696b6679fcb8147f7fd51ba0b45cc7a214e
nikoskalai <nikoskalai@gmail.com>
2017-06-14 17:18:44 +0300

    Created Repository

---
+++ Auto Backup/src/folder/backup/Folder.java
@@ -0,0 +1,15 @@
+
+package folder.backup;
+
+import java.io.File;
+
+public class Folder {
+    public int id;
+    public File file;
+
+    public Folder(int id, File file) {
+        this.id = id;
+        this.file = file;
+    }
+    
+}
